54e97c4095fc19791b33436009070444a29f8630
Xavier0301 <xavier.servot0@gmail.com>
2023-04-18 18:30:37 +0200

    Implements batching and other misc features requires for UpMem integration. Batching passes diff tests.

--- src/batch.c
+++ src/batch.c
@@ -9,5 +9,7 @@ void batch_hashing(tensor3d_t* resulting_hashes, model_t* model, bmatrix_t* inpu
 }
 
 void batch_prediction(size_t* results, model_t* model, bmatrix_t* input_batch, size_t batch_size) {
-
+    for(size_t it = 0; it < batch_size; ++it) {
+        results[it] = model_predict2(model, MATRIX_AXIS1(*input_batch, it));
+    }
 }
