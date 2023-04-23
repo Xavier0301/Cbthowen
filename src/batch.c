19864466354bfeddcb20c2c4ee6fbac319660e88
Xavier0301 <xavier.servot0@gmail.com>
2023-04-23 15:27:18 +0200

    Fixes a very vicious vicious bug: wrong stride in tmp matrix buffer that serves as a pointer to tensor element.

--- src/batch.c
+++ src/batch.c
@@ -1,7 +1,7 @@
 #include "batch.h"
 
 void batch_hashing(tensor3d_t* resulting_hashes, model_t* model, bmatrix_t* input_batch, size_t batch_size) {
-    matrix_t tmp_hashes = { .stride = model->num_filters, .data=NULL };
+    matrix_t tmp_hashes = { .stride = model->filter_hashes, .data=NULL };
     for(size_t it = 0; it < batch_size; ++it) {
         tmp_hashes.data = TENSOR3D_AXIS1(*resulting_hashes, it);
         perform_hashing(tmp_hashes, model, MATRIX_AXIS1(*input_batch, it));
